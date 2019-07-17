5536290e1112018317bc8c8c79e2c5833f73b362
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2019-07-17 11:33:09 +0700

    little changes

--- app/Models/Question.js
+++ app/Models/Question.js
@@ -1,14 +1,10 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Question extends Model {
-    // answers () {
-    //     return this.hasMany('App/Models/Answer')
-    // }
     users() {
-    	return this.belongsToMany('App/Models/User').pivotTable('answers')
+    	return this.belongsToMany('App/Models/User').pivotTable('answers').withTimestamps()
     }
 }
 
--- app/Models/User.js
+++ app/Models/User.js
@@ -1,9 +1,7 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-/** @type {import('@adonisjs/framework/src/Hash')} */
 const Hash = use('Hash')
 
 class User extends Model {
@@ -11,10 +9,6 @@ class User extends Model {
   static boot() {
     super.boot()
 
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
     this.addHook('beforeSave', async userInstance => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
@@ -22,28 +16,15 @@ class User extends Model {
     })
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
   tokens() {
     return this.hasMany('App/Models/Token')
   }
-
-  // answers () {
-  //   return this.hasMany('App/Models/Answer')
-  // }
 
   questions() {
     return this.belongsToMany('App/Models/Question')
                .pivotTable('answers')
                .withPivot(['answer', 'attachment'])
+               .withTimestamps()
   }
 
 }
